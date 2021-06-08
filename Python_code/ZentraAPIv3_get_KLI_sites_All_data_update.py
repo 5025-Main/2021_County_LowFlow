73e242ad6ababeb3266e0e85356136daa240d170
Messina <alex.messina@amecfw.com>
2021-06-08 16:50:05 -0700

    starting on QC'd data

---
+++ Python_code/ZentraAPIv3_get_KLI_sites_All_data_update.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 15:44:03 2021
+
+@author: alex.messina
+"""
+
+import requests
+import json
+import pandas as pd
+import datetime as dt
+import time
+import numpy as np
+
+startTime = dt.datetime.now()
+
+## Read in existing Wood data
+all_level_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Level_data_1.csv',index_col=0, parse_dates=True)
+all_level_dat = all_level_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+all_temp_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Temp_data_1.csv',index_col=0, parse_dates=True)
+all_temp_dat = all_temp_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+all_cond_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Cond_data_1.csv',index_col=0, parse_dates=True)
+all_cond_dat = all_cond_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+#%%
+## Read in existing KLI data
+ex_kli_level_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Level_data_KLI_1.csv',index_col=0, parse_dates=True)
+ex_kli_level_dat = ex_kli_level_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+ex_kli_temp_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Temp_data_KLI_1.csv',index_col=0, parse_dates=True)
+ex_kli_temp_dat = ex_kli_temp_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+ex_kli_cond_dat = pd.read_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Cond_data_KLI_1.csv',index_col=0, parse_dates=True)
+ex_kli_cond_dat = ex_kli_cond_dat.reindex(pd.date_range(dt.datetime(2021,5,1),dt.datetime(2021,9,16,0,0),freq='5Min'))
+
+#%%
+
+### START/END DATES
+today = dt.datetime.now()
+days_ago = today - dt.timedelta(days=2) ## get data from past n days
+startdate = days_ago.strftime("%m-%d-%Y")
+enddate = today.strftime("%m-%d-%Y")
+
+## Get start date for new data as the last date in the existing data. Duplicate data will be overwritten with new data
+startdate = ex_kli_level_dat.dropna(how='all').index[-1].strftime("%m-%d-%Y")
+
+# or hardcode start/end
+#startdate = '06-3-2021'
+#enddate = '05-13-2021'
+
+#%%
+
+## GET DATA FUNCTION
+def get_zentra_df(url, params, headers, retries=3):
+    ## Initial request
+    response = requests.get(url, params=params, headers=headers)
+    ## If good response [200] then parse data and return the df
+    if response.ok:
+        print ('Succesful response.')
+        content = json.loads(response.content)
+        if content['pagination']['page_num_readings']>0:
+            df = pd.read_json(content['data'], convert_dates=False, orient='split')
+            print (df[['datetime', 'mrid', 'measurement','value', 'units']].head())
+            print (df[['datetime', 'mrid', 'measurement','value', 'units']].tail())
+        else:
+            df = pd.DataFrame()
+    ## If no good response [429], need to wait and try again
+    else:
+        success = False #False til good response
+        fails = 1 #set fails to 1 for first loop
+        fail_count = retries # 3 retries by default
+        while fails <= fail_count and  success==False:
+            print ('Failed '+str(fails)+' time(s). '+str(response))
+            print ('Waiting to retry...')
+            time.sleep(65)
+            print ('trying....')
+            response = requests.get(url, params=params, headers=headers)
+            ## If good response after waiting 65 sec...
+            if response.ok:
+                success = True
+                content = json.loads(response.content)
+                if content['pagination']['page_num_readings']>0:
+                    df = pd.read_json(content['data'], convert_dates=False, orient='split')
+                    print ('Device data: ')
+                    print (df[['datetime', 'mrid', 'measurement','value', 'units']].head())
+                    print (df[['datetime', 'mrid', 'measurement','value', 'units']].tail())
+                else:
+                    df = pd.DataFrame()
+            else:
+                print ('Failed '+str(response))
+                pass
+            fails +=1 ## count fails up 1 every loop
+        ## If 3 tries are all fails raise error
+        if fails > fail_count and success==False:
+            raise Exception(response)
+            print ('Failed after '+str(retries)+' response: '+str(response))
+        if success == True:
+            print ('Successful after '+str(fails)+' tries')
+            pass
+    return df, content
+
+### DEVICES
+site_device_dict = {
+                  'SDG-074':('06-02238',1),
+                  'SDG-074F':('06-02296',1),
+                  'SDG-074K':('06-02284',1),
+                  'SDG-077':('06-02206',1),
+                  'SDG-077E':('06-02349',1),
+                  'SDG-080':('06-02223',1),
+                  'SDG-577':('06-02208',1),
+                  'SDG-579':('06-02237',1),
+                  'SMG-015':('06-02225',1),
+                  'SMG-062':('06-02236',3),
+                  'SMG-062D':('06-02195',1),
+                  'SMG-098':('06-02202',1)
+                  }
+
+                    
+device_site_dict= {
+                '06-02238':(['SDG-074'],1),
+                '06-02296':(['SDG-074F'],1),
+                '06-02284':(['SDG-074K'],1),
+                '06-02206':(['SDG-077'],1),
+                '06-02349':(['SDG-077E'],1),
+                '06-02223':(['SDG-080'],1),
+                '06-02208':(['SDG-577'],1),
+                '06-02237':(['SDG-579'],1),
+                '06-02225':(['SMG-015'],1),
+                '06-02236':(['SMG-062'],3),
+                '06-02195':(['SMG-062D'],1),
+                '06-02202':(['SMG-098'],1)
+                }
+
+
+#device_site_dict= {
+#                '06-02192':(['CAR-059'],3)}
+
+
+
+## Empty dataframes for new data
+level_dat = pd.DataFrame(index=pd.date_range(startdate,enddate,freq='5Min'))
+temp_dat = pd.DataFrame(index=pd.date_range(startdate,enddate,freq='5Min'))
+cond_dat = pd.DataFrame(index=pd.date_range(startdate,enddate,freq='5Min'))
+## iterate over loggers, can get 60 loggers in 1 min from API or else throttled and have to wait 1min
+for device_sn, (site_s, port_s) in device_site_dict.items():
+    print()
+    print ('Device: '+device_sn)
+    
+    ### API PARAMS
+    #token = "Token {TOKEN}".format(TOKEN="5570f23feeb666615003051140cea73ccdb18639") ## Wood token
+    token = "Token {TOKEN}".format(TOKEN="0e599fae024923f65b51a73cbb19ee94ae729444") ## KLI token
+    url = "https://zentracloud.com/api/v3/get_readings/"
+    headers = {'content-type': 'application/json', 'Authorization': token}
+    output_format = "df"
+    perpage = 2000
+    params = {'device_sn': device_sn, 'output_format': output_format,'per_page':2000,'sort_by':'ascending','start_date':startdate,'end_date':enddate}
+
+    device_dat = pd.DataFrame()
+    ## FIRST PAGE OF DATA
+    df, content = get_zentra_df(url, params=params, headers=headers)
+    
+    if len(df) > 0:
+        df['datetime'] = pd.to_datetime(df['datetime'],utc=True).dt.tz_convert(tz='US/Pacific').dt.tz_localize(None) # in case some aren't on PST
+        df.index = df['datetime']
+        
+        device_dat = device_dat.append(df)
+        
+    #    ## NEXT PAGE(S)
+    #    length_data = content['pagination']['page_num_readings']
+    #    next_url = content['pagination']['next_url']
+    #    ## loop until a page returns with no data
+    #    while length_data > 0:
+    #        next_df, next_content = get_zentra_df(next_url, params, headers, retries=3)
+    #        
+    #        if next_content['pagination']['page_num_readings'] > 0: ## if it actually returns data, if not df is empty
+    #            next_df['datetime'] = pd.to_datetime(next_df['datetime'],utc=True).dt.tz_convert(tz='US/Pacific').dt.tz_localize(None) # in case some aren't on PST
+    #            next_df.index = next_df['datetime']
+    #            device_dat = device_dat.append(next_df)
+    #            ## update length data and next_url with this page's values
+    #            length_data = next_content['pagination']['page_num_readings']
+    #            next_url = next_content['pagination']['next_url']
+    #        else:
+    #            length_data = 0
+        
+        ## For each logger, if multiple sites then iterate over sites
+        for site in site_s:
+            port_num = site_device_dict[site][1]
+            print ('Saving data for...')
+            print (site, port_num)
+            
+            site_data = device_dat[device_dat['port_num']==port_num]
+            
+            site_level_data = site_data[site_data['measurement']=='Water Level']      [['datetime','mrid', 'measurement','value', 'units']]
+            site_temp_data = site_data[site_data['measurement']=='Water Temperature'] [['datetime','mrid', 'measurement','value', 'units']]
+            site_cond_data = site_data[site_data['measurement']=='EC']                [['datetime','mrid', 'measurement','value', 'units']]
+        
+            level_dat[site+'_level_in'] = site_level_data.drop_duplicates(subset=['datetime']).dropna(how='all')['value']
+            temp_dat[site+'_temp_F'] = site_temp_data.drop_duplicates(subset=['datetime']).dropna(how='all')['value']
+            cond_dat[site+'_cond_mScm'] = site_cond_data.drop_duplicates(subset=['datetime']).dropna(how='all')['value']
+            
+            #level_dat = level_dat.replace(2579.29, np.nan)
+            #temp_dat = temp_dat.replace(1121.6, np.nan)  
+            #cond_dat = cond_dat.replace(3.4197944250000004e+62, np.nan) 
+            
+            level_dat = np.round(level_dat / 25.4,2).replace(2579.29, np.nan)
+            temp_dat = np.round((temp_dat * 9/5)+32, 1).replace(1121.5, np.nan)  
+            cond_dat = cond_dat.replace(3.4197944250000004e+62, np.nan) 
+            
+            ex_kli_level_dat.loc[level_dat.index,site+'_level_in'] = level_dat[site+'_level_in']
+            ex_kli_temp_dat.loc[temp_dat.index,site+'_temp_F'] = temp_dat[site+'_temp_F']
+            ex_kli_cond_dat.loc[cond_dat.index,site+'_cond_mScm'] = cond_dat[site+'_cond_mScm']
+    
+#Python 3: 
+print ('Script downloaded data in:')
+print(dt.datetime.now() - startTime)   
+    
+    
+
+
+#%%
+## drop na and save to csv
+    
+ex_kli_level_dat = ex_kli_level_dat.dropna(how='all')
+ex_kli_level_dat.to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Level_data_KLI_1.csv')
+ex_kli_temp_dat = ex_kli_temp_dat.dropna(how='all')
+ex_kli_temp_dat.to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Temp_data_KLI_1.csv')
+ex_kli_cond_dat = ex_kli_cond_dat.dropna(how='all')
+ex_kli_cond_dat.to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Cond_data_KLI_1.csv')
+    
+#%%
+
+## Append to Wood data
+for col in ex_kli_level_dat:
+    print (col)
+    all_level_dat.loc[ex_kli_level_dat.index, col] = ex_kli_level_dat[col]
+    
+for col in ex_kli_temp_dat:
+    print (col)
+    all_temp_dat.loc[ex_kli_temp_dat.index, col] = ex_kli_temp_dat[col]
+
+for col in ex_kli_cond_dat:
+    print (col)
+    all_cond_dat.loc[ex_kli_cond_dat.index, col] = ex_kli_cond_dat[col]    
+
+
+#%%
+    
+    ## Read in existing Wood data
+all_level_dat.dropna(how='all').to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Level_data_1.csv')
+
+
+all_temp_dat.dropna(how='all').to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Temp_data_1.csv')
+
+
+all_cond_dat.dropna(how='all').to_csv('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/PowerBI/County2021/Flow_data_from_API_v3/'+'Cond_data_1.csv')
---
+++ Python_code/get OneRain_data_2021.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 06 12:30:18 2016
+
+@author: alex.messina
+"""
+
+import time
+
+from bs4 import BeautifulSoup
+
+import numpy as np
+import pandas as pd
+import requests
+
+import datetime as dt
+import os
+
+maindir = 'C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/'
+os.chdir('C:/Users/alex.messina/Documents/GitHub/2021_County_LowFlow/Python_code/')
+
+## Rain gauges indexed by rain gauge name
+Rain_gauge_info = pd.DataFrame.from_csv(maindir+'Ancillary_files/Rain_gauge_info.csv')
+## List of unique rain gauge names
+Rain_gauge_names = Rain_gauge_info.index.unique()
+
+
+######### UPDATE HERE ###################
+#start_date, end_date = '2021-05-01', '2020-06-29' 
+start_date, end_date = '2021-05-01', dt.date.today().strftime('%Y-%m-%d') ## for current day: dt.date.today().strftime('%Y-%m-%d')
+time_bin  = '3600' #seconds. Daily=86400, Hourly=3600
+#######################################
+
+
+daterange = pd.date_range(start_date.replace('-',''),dt.datetime.now(),freq='D')
+
+
+for Rain_gauge_name in Rain_gauge_names:
+    print (Rain_gauge_name)
+    RG_ID = str(Rain_gauge_info.ix[Rain_gauge_name]['rain_gauge_id'])
+    print ('ID: '+ RG_ID)
+    RG_SERIAL = Rain_gauge_info.ix[Rain_gauge_name]['rain_gauge_serial']
+    print ('SERIAL :'+RG_SERIAL)
+    RG_DEV_ID = str(Rain_gauge_info.ix[Rain_gauge_name]['rain_gauge_device_id'])
+    print ('DEVICE ID: '+ RG_DEV_ID)
+    RG_DEV_SERIAL = Rain_gauge_info.ix[Rain_gauge_name]['rain_gauge_device_serial']
+    print ('DEVICE SERIAL: '+ RG_DEV_SERIAL)
+    print ('\n')
+
+    site_df = pd.DataFrame()
+    try:
+        ## Verify rain gauge name
+        url = 'https://sandiego.onerain.com/sensor.php?time_zone=US%2FPacific&site_id='+RG_ID+'&site='+RG_SERIAL+'&device_id='+RG_DEV_ID+'&device='+RG_DEV_SERIAL+'&bin='+time_bin+'&range=Custom+Range&legend=true&thresholds=true&refresh=off&show_raw=true&show_quality=true&data_start='+start_date+'+00%3A00%3A00&data_end='+end_date+'+23%3A59%3A59'
+        
+        s = requests.get(url).content
+        
+        soup = BeautifulSoup(s)
+        
+        try:
+            print ()
+            print ('Grabbing data for Rain gauge: '+Rain_gauge_name)
+            print ('...from date range: '+str(daterange[0])+' to '+str(daterange[-1]))
+            try:
+                ##enter web address of data
+                #start_date = str(month.year)+'-'+str(month.month)+'-'+'01'
+                #end_date = str(month.year)+'-'+str(month.month)+'-'+str(month.day)
+                #start_date, end_date = str(date),str(date+ dt.timedelta(days=1))
+
+                s = requests.get(url).content
+                soup = BeautifulSoup(s)
+                
+                ## Check if this is rain increment data
+               
+                #if 'Rain Increment' in soup.findAll('h1')[0].text:   #oldway
+                if 'Rain Increment' in soup.h3.text:
+                    print ('Rain Increment in Title...')
+                    #print url
+                    ## Get data from website
+                    times, rain_vals = [], []
+                    ## Get times
+                    #for h4_tag in soup.findAll('h4',{'class':'status-inline list-group-item-heading'}):
+                    #    print(h4_tag)
+                    for h4_tag in soup.findAll('li',{'class':'list-group-item'}):
+                        print (h4_tag)
+                        try:
+                            datetime =  h4_tag.find('span', {'class':'text-nowrap'}).text
+                            #print datetime
+                            times.append(datetime)
+                        except:
+                           print ('Failed')
+                           print (h4_tag)
+                           print ()
+                           pass
+                    ## Get rain data
+                    #for h4_tag in soup.findAll('h4',{'class':'list-group-item-heading'}):
+                        
+                        try:
+                            rain_val =  h4_tag.text.split('&nbsp;')[0]
+                            #print rain_val
+                            rain_vals.append(float(rain_val))
+                        except:
+                           print ('Failed')
+                           print (h4_tag)
+                           print ()
+                           pass
+                    rain_data_df = pd.DataFrame({'Rain_in':rain_vals},index=pd.to_datetime(times)).sort_index()
+                    rain_data_1hr = rain_data_df.resample('1H').sum().fillna(0.) 
+                    rain_data_1D = rain_data_df.resample('D').sum()
+                    
+                    rain_data_1hr.to_csv(maindir+'Rain_data/'+Rain_gauge_name+'_hourly.csv')
+                    rain_data_1D.to_csv(maindir+'Rain_data/'+Rain_gauge_name+'_daily.csv')
+            except Exception as e:
+               print (e)
+               pass
+        except Exception as e:
+                print (e)
+                pass
+    except Exception as e:
+                print (e)
+                pass
+            
+
+        
+#%%combine mutiple files
+
+
+#Script expects the old files to be in a folder in the Raw data directory within the "0 - Rain Data". 
+#Place newly downloaded rain data in a second folder in the same Raw data directory.
+#The script will combine the files with the same names from each folder and output them to the raw data directory.
+
+raindir = maindir+'Rain_data/'
+
+for old_filename in os.listdir('C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/Rain_data/May rain data'):
+    print old_filename
+    rain_panda = pd.DataFrame(columns=['Rain_in'])
+    for f in [x[0] for x in os.walk(raindir)]:
+        print f
+        ## month file
+        try:
+            old_file = pd.read_csv(f  +'/'+ old_filename, index_col = 0)
+            print(old_filename)
+            rain_panda = rain_panda.append(old_file)
+        except:
+            pass
+        
+        
+    rain_panda['idx']=rain_panda.index
+    rain_panda = rain_panda.drop_duplicates(subset='idx')
+    rain_panda = rain_panda.sort_index()
+#    print(rain_panda)
+    rain_panda[['Rain_in']].to_csv(raindir+old_filename)
+
+    
+    
+    
+
+#%% Plot rain data
+## Data from  https://sandiego.onerain.com/rain.php
+raindir = maindir+'Rain_data/'
+#for all gauges
+rain_files = [f for f in os.listdir(maindir+'Rain_data/') if f.endswith('daily.csv')==True]
+#for one gauge
+#gauge_name = 'Flinn_Springs'
+#rainfiles = [f for f in os.listdir(maindir+'Rain_data/') if f.endswith('daily.csv')==True]
+#rainfiles = [f for f in os.listdir(maindir+'Rain_data/') if f.endswith('hourly.csv')==True]
+
+fig, ax = plt.subplots(1,1,figsize=(12,8))
+
+for rainfile in rain_files:
+    print ('')
+    print 'Precip file: '+rainfile
+    rain = pd.read_csv(raindir+rainfile,index_col=0)
+    rain.index = pd.to_datetime(rain.index)
+    ## Resample to regular interval and fill non-data with zeros
+    #rain = rain.resample('15Min').sum()
+    
+
+    ax.plot_date(rain.index, rain['Rain_in'],ls='steps-pre',marker='None',label=rainfile.split('_daily.csv')[0])
+    ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%m/%d/%Y'))
+    
+    
+    #rain_1D.to_csv(daily_rain_files+'Daily-'+rainfile.replace('.xls','.csv'))
+ax.set_ylabel('Rain in. (daily)')
+ax.legend(ncol=4,fontsize=12)
+plt.tight_layout()
+
